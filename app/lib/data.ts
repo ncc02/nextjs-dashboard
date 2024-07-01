53ceb803ef3310ec5afc727ef065b9a361dc5120
ncc02 <cuongtk2002@gmail.com>
2024-07-02 04:48:42 +0700

    finish_chapter10

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -30,6 +30,8 @@ export async function fetchRevenue() {
 
 export async function fetchLatestInvoices() {
   try {
+    // console.log('Fetching revenue data...');
+    // await new Promise((resolve) => setTimeout(resolve, 1000));
     const data = await sql<LatestInvoiceRaw>`
       SELECT invoices.amount, customers.name, customers.image_url, customers.email, invoices.id
       FROM invoices
